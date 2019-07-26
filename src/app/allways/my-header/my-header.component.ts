55c38f137d434a856f475f0949057b7b51818f86
munich99 <regerstrasse@icloud.com>
2019-07-26 19:52:49 +0200

    navi width dynamic value

--- src/app/allways/my-header/my-header.component.ts
+++ src/app/allways/my-header/my-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { navigation } from './navi'
 
 @Component({
   selector: 'app-my-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyHeaderComponent implements OnInit {
 
+  navigation = navigation;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeLanguage:boolean=true;
 }
---
+++ src/app/allways/my-header/navi.ts
@@ -0,0 +1,15 @@
+export const navigation=[
+    {
+        navi: "about me",
+        deutsch: "Das bin ich",
+        ziel: "start",
+        id: 1
+      },
+      {
+        navi: "contact me",
+        deutsch: "Schreibe mir",
+        ziel: "contact",
+        id: 2
+      }
+
+  ]
